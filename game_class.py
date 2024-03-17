74fdb2dfb624e0e5b413dfecd855e2bd1faa7961
Maysan <maysanhalaby4305@gmail.com>
2024-03-17 14:51:35 +0200

    corrected game class

--- game_class.py
+++ game_class.py
@@ -1,5 +1,5 @@
 """
-    Create an abstract class
+    Purpose: Create an abstract class.
     Author: Maysan Halaby
     Date: 17/03/2024
 """
@@ -8,32 +8,32 @@ from abc import ABC, abstractmethod
 
 class Game(ABC):
     """
-    Abstract class that represent a game
+    Abstract class that represent a game.
     """
     @abstractmethod
     def start_game(self):
         """
-        starts the game
+        Starts the game.
         """
         pass
 
     @abstractmethod
     def get_player_move(self):
         """
-        gets the player move by input
+        Gets the player move by input.
         """
         pass
 
     @abstractmethod
     def check_win(self):
         """
-        Checks if the player has won the game
+        Checks if the player has won the game.
         """
         pass
 
     @abstractmethod
     def end_game(self):
         """
-        Ends the game
+        Ends the game.
         """
         pass
